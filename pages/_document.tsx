7fc619847009301fa2b64133d93d227f147652ed
AdaptCharm <jamie@jamie.no>
2021-09-17 07:21:49 +0200

    Initial upload

---
+++ pages/_document.tsx
@@ -0,0 +1,28 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html className='h-100' lang='en'>
+        <Head>
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link
+            rel='preconnect'
+            href='https://fonts.gstatic.com'
+            crossOrigin='true'
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Lato&family=Playfair+Display:ital,wght@1,700&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
+        <body className='h-100 bg-gradient loading'>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
