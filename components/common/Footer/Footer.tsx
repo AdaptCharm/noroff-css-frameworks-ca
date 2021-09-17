c77fd000cbd3876dfc934dd7cd2bbf812b91f994
AdaptCharm <jamie@jamie.no>
2021-09-17 15:40:25 +0200

    Add footer component

---
+++ components/common/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react'
+
+import { Vimeo, Youtube } from '@components/icons'
+
+const Footer: FC = () => {
+  return <footer className='bg-white py-3 mt-auto'></footer>
+}
+
+export default Footer
