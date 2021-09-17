045373c90eff1b023d1602845d52193b62bb5f7b
AdaptCharm <jamie@jamie.no>
2021-09-17 07:24:39 +0200

    Minor changes to the app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import '@assets/main.css'
+
+import { FC, useEffect } from 'react'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const Noop: FC = ({ children }) => <>{children}</>
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const Layout = (Component as any).Layout || Noop
+
+  useEffect(() => {
+    document.body.classList?.remove('loading')
+  }, [])
+
+  return (
+    <>
+      <Layout pageProps={pageProps}>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
+
 export default MyApp
