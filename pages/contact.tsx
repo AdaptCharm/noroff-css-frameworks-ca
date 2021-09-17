4a296eab43bcdb7dd633f15af16eb3a67409a7cb
AdaptCharm <jamie@jamie.no>
2021-09-17 15:37:25 +0200

    Add required pages

---
+++ pages/contact.tsx
@@ -0,0 +1,13 @@
+import { Layout } from '@components/common'
+
+const Contact = () => {
+  return (
+    <>
+      <h1>Contact page</h1>
+    </>
+  )
+}
+
+export default Contact
+
+Contact.Layout = Layout
---
+++ pages/news.tsx
@@ -0,0 +1,13 @@
+import { Layout } from '@components/common'
+
+const News = () => {
+  return (
+    <>
+      <h1>News page</h1>
+    </>
+  )
+}
+
+export default News
+
+News.Layout = Layout
